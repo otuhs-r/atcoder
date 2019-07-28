7252e242af7cd5f0538b9ffa7c595b42f4bc781d
otuhs_d <yekcim.114@gmail.com>
2019-07-28 15:14:47 +0900

    refactoring

--- abc135/B.rb
+++ abc135/B.rb
@@ -1,13 +1,3 @@
 n = gets.to_i
 p = gets.split.map(&:to_i)
-
-p.each_with_index do |n, i|
-  if n != i + 1
-    tmp = p.find_index(i + 1)
-    p[i] = i + 1
-    p[tmp] = n
-    break
-  end
-end
-
-puts p == (1..n).to_a ? 'YES' : 'NO'
+puts p.each.with_index(1).count { |n, i| n != i } <= 2 ? 'YES' : 'NO'
