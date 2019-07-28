7af087f9a2effaaf5a22d51e5b35695540346a50
otuhs_d <yekcim.114@gmail.com>
2019-07-28 14:50:11 +0900

    refactoring

--- abc135/C.rb
+++ abc135/C.rb
@@ -1,20 +1,11 @@
 n = gets.to_i
 a, b = 2.times.map { gets.split.map(&:to_i) }
 
-answer = b.each_with_index.reduce(0) do |s, (y, i)|
-  if a[i] < y
-    tmp = y - a[i]
-    if a[i + 1] - tmp >= 0
-      a[i + 1] -= tmp
-      s += a[i] + tmp
-    else
-      tmp2 = a[i + 1]
-      a[i + 1] = 0
-      s += a[i] + tmp2
-    end
-  else
-    s += y
-  end
+answer = (0...n).reduce(0) do |s, i|
+  x = [a[i], b[i]].min
+  y = [a[i + 1], b[i] - x].min
+  a[i + 1] -= [b[i] - x, a[i + 1]].min
+  s += x + y
 end
 
 puts answer
