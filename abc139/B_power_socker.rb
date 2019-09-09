e5f3f3338bbec4e15dcba811be49e03cbc9b0640
otuhs_d <yekcim.114@gmail.com>
2019-09-09 20:34:56 +0900

    リファクタリング

--- abc139/B_power_socker.rb
+++ abc139/B_power_socker.rb
@@ -1,9 +1,2 @@
 a, b = gets.split.map(&:to_i)
-
-answer = if a == b
-  1
-else
-  (b - a) % (a - 1) == 0 ? (b - a) / (a - 1) + 1 : (b - a) / (a - 1) + 2
-end
-
-puts answer
+puts ((b - 1) / (a - 1.0)).ceil
