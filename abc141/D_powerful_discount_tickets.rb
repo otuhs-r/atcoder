6d2dd3bb0c8fbec7d1d6d2633dd6cba2bec02a84
otuhs_d <yekcim.114@gmail.com>
2019-09-20 23:51:30 +0900

    Priority Queue で実装したが 2 つ TLE

--- abc141/D_powerful_discount_tickets.rb
+++ abc141/D_powerful_discount_tickets.rb
@@ -1,4 +1,7 @@
 _n, m = gets.split.map(&:to_i)
-a = gets.split.map(&:to_i)
-m.times { a[a.index(a.max)] /= 2 }
+a = gets.split.map(&:to_i).sort
+m.times do
+  h_max = a.pop / 2
+  a.insert(a.bsearch_index { |x| x >= h_max } || 0, h_max)
+end
 p a.inject(:+)
