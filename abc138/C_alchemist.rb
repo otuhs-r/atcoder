d99e2bfef7eb4cd550f824dbbcd2a263ed803afd
otuhs_d <yekcim.114@gmail.com>
2019-09-25 22:40:31 +0900

    無駄を省いた

--- abc138/C_alchemist.rb
+++ abc138/C_alchemist.rb
@@ -1,9 +1,2 @@
-n = gets.to_i
-v = gets.split.map(&:to_i).sort
-
-(n - 1).times do
-  tmp = (v.shift + v.shift) / 2.0
-  v.insert(v.bsearch_index { |z| z >= tmp } || 0, tmp)
-end
-
-puts v[0]
+gets
+p gets.split.map(&:to_f).sort.inject { |s, i| (s + i) / 2 }
