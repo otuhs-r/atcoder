131ca580ac7aef36caaab06740b80f578c8a5cd8
otuhs_d <yekcim.114@gmail.com>
2019-09-26 21:44:45 +0900

    解説確認。after_contestはWA

--- abc138/D_ki.rb
+++ abc138/D_ki.rb
@@ -1,22 +1,8 @@
-class Node
-  attr_accessor :counter, :children
-
-  def initialize
-    @counter = 0
-    @children = []
-  end
-
-  def add(x)
-    @children.each { |c| c.add(x) }
-    @counter += x
-  end
-end
-
 n, q = gets.split.map(&:to_i)
-edges = (n - 1).times.map { gets.split.map(&:to_i) }
+edges = (n - 1).times.map { gets.split.map(&:to_i) }.sort { |(a1, b1),(a2, b2)| a1 == a2 ? b1 <=> b2 : a1 <=> a2 }
 points = q.times.map { gets.split.map(&:to_i) }
 
-nodes = n.times.map { Node.new }
-edges.each { |(a, b)| nodes[a - 1].children << nodes[b - 1] }
-points.each { |(p, x)| nodes[p - 1].add(x) }
-puts nodes.map { |node| node.counter }.join(' ')
+c = [0] * n
+points.each { |(p, x)| c[p - 1] += x }
+edges.each { |(a, b)| c[b - 1] += c[a - 1] }
+puts c.join(' ')
