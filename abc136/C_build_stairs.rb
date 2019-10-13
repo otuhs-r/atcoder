2ef9a83b066f7518ffef5574238c33fd9ace2cbc
otuhs_d <yekcim.114@gmail.com>
2019-10-13 15:46:35 +0900

    初見WA

---
+++ abc136/C_build_stairs.rb
@@ -0,0 +1,10 @@
+n = gets.to_i
+h = gets.split.map(&:to_i)
+(1...(n - 1)).each do |i|
+  h[i] -= 1 if h[i] > h[i + 1]
+  if (h[i - 1] > h[i]) || (h[i] > h[i + 1])
+    puts 'No'
+    exit
+  end
+end
+puts 'Yes'
