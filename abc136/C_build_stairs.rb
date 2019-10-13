e694327023786ad6f266d9f20aef60873b11a57f
otuhs_d <yekcim.114@gmail.com>
2019-10-13 15:48:03 +0900

    AC: 段を下げられるところは下げておく

--- abc136/C_build_stairs.rb
+++ abc136/C_build_stairs.rb
@@ -1,7 +1,7 @@
 n = gets.to_i
 h = gets.split.map(&:to_i)
 (1...(n - 1)).each do |i|
-  h[i] -= 1 if h[i] > h[i + 1]
+  h[i] -= 1 if h[i] > h[i + 1] || h[i] - h[i - 1] > 0
   if (h[i - 1] > h[i]) || (h[i] > h[i + 1])
     puts 'No'
     exit
